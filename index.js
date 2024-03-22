f7f6b610f1b2cfcfd018c93e8fb87fdd1e3848ae
borygashill <borygashill608@gmail.com>
2024-03-22 11:55:52 +0900

    로그인 구현

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
-const title = document.querySelector("#title");
+const loginform = document.querySelector("#login-form");
+const logininput = document.querySelector("#login-input");
+const greeting = document.querySelector("#greeting");
+const alerting = document.querySelector("#alert");
 
-console.log(title);
+const onLoginSubmit = (e) =>{
+    e.preventDefault();
 
-const pp = document.querySelector("#pp");
+    const userName = logininput.value;
+    if(!userName){
+        alerting.classList.remove("hidden");
+        alerting.innerText = "이름을 입력해주세요";
+    }else{
+        alerting.classList.add("hidden");
+        loginform.classList.add("hidden");
 
-pp.classList.add("test")
+        greeting.innerText = `Hello! ${userName}`;
+        greeting.classList.remove("hidden");
+        localStorage.setItem("username", userName);
+    }
+}
+
+const savedUsername = localStorage.getItem('username');
+
+if(!savedUsername){
+    loginform.classList.remove("hidden");
+    loginform.addEventListener("submit", onLoginSubmit);
+}else{
+    greeting.classList.remove("hidden");
+    greeting.innerText = `Hello! ${savedUsername}`;
+}
+
